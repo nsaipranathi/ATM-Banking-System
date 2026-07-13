b4d79b8071bcaf43bf8524a66fb5f415e4afcd41
N. Sai Pranathi <149822584+nsaipranathi@users.noreply.github.com>
2026-07-13 20:07:11 +0530

    Delete student_app directory

--- student_app/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE student_db;
-USE student_db;
-CREATE TABLE students (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    age INT,
-    subject VARCHAR(50),
-    marks INT
-);
